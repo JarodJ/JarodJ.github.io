1ffcffcf7393315faa315670963f5f408ca9bc03
JarodJ <31993476+JarodJ@users.noreply.github.com>
2021-03-15 10:58:41 -0400

    update YouTube.user.js

--- YouTube.user.js
+++ YouTube.user.js
@@ -1,11 +1,4 @@
-// ==UserScript==
-// @name         YouTube
-// @match        https://www.youtube.com/watch?*
-// @match        https://gaming.youtube.com/watch?*
-// @grant        none
-// ==/UserScript==
-
 (function()
 	{
-	alert("running");
+	alert("test");
 	})();
